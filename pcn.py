2a186082420d36e20367c3b195035063003c497c
WYSIATI <chesterlee@Chesters-MacBook-Air.local>
2014-10-21 22:43:53 -0300

    update kfold and pcn

--- pcn.py
+++ pcn.py
@@ -78,26 +78,35 @@ class pcn:
 	def confmat(self,inputs,targets):
 		"""Confusion matrix"""
 
-		# Add the inputs that match the bias node
-		inputs = np.concatenate((inputs,-np.ones((self.nData,1))),axis=1)
-		outputs = np.dot(inputs,self.weights)
+		outputs = self.pcnfwd(inputs)
+		outputs = outputs[0, :]
+		nClasses = 3
+
+		cm = zeros((nClasses, nClasses))
+		temp_idx = np.where(targets == 1)
+		cm[temp_idx[0][0], :] = outputs
+		return cm
+
+		# # Add the inputs that match the bias node
+		# inputs = np.concatenate((inputs,-np.ones((self.nData,1))),axis=1)
+		# outputs = np.dot(inputs,self.weights)
 	
-		nClasses = np.shape(targets)[1]
+		# nClasses = np.shape(targets)[1]
 
-		if nClasses==1:
-			nClasses = 2
-			outputs = np.where(outputs>0,1,0)
-		else:
-			# 1-of-N encoding
-			outputs = np.argmax(outputs,1)
-			targets = np.argmax(targets,1)
+		# if nClasses==1:
+		# 	nClasses = 2
+		# 	outputs = np.where(outputs>0,1,0)
+		# else:
+		# 	# 1-of-N encoding
+		# 	outputs = np.argmax(outputs,1)
+		# 	targets = np.argmax(targets,1)
 
-		cm = np.zeros((nClasses,nClasses))
-		for i in range(nClasses):
-			for j in range(nClasses):
-				cm[i,j] = np.sum(np.where(outputs==i,1,0)*np.where(targets==j,1,0))
+		# cm = np.zeros((nClasses,nClasses))
+		# for i in range(nClasses):
+		# 	for j in range(nClasses):
+		# 		cm[i,j] = np.sum(np.where(outputs==i,1,0)*np.where(targets==j,1,0))
 
-		print cm
-		print np.trace(cm)/np.sum(cm)
+		# print cm
+		# print np.trace(cm)/np.sum(cm)
 
 
