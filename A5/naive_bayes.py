d863d243647d7fc5707e13b0340935985de014b7
WYSIATI <chester.lee.cold@gmail.com>
2014-11-01 17:07:03 -0300

    Used logarithm in bayes rules and got approximately 93.9% accuracy at the best case

--- A5/naive_bayes.py
+++ A5/naive_bayes.py
@@ -6,98 +6,117 @@ import scipy.io
 import math
 
 # Get data from a matlab file NewsGroup.mat.
-data = scipy.io.loadmat('NewsGroup.mat')
-train_data = data.get('TRAIN_DATA')
-train_label = data.get('TRAIN_LABEL')
-test_data = data.get('TEST_DATA')
-test_label = data.get('TEST_LABEL')
+g_data = scipy.io.loadmat('NewsGroup.mat')
+g_train_data = g_data.get('TRAIN_DATA')
+g_test_data = g_data.get('TEST_DATA')
+g_test_label = g_data.get('TEST_LABEL')
 
-words_in_class1 = []
-words_in_class2 = []
-for element in train_data:
-	if element[0] < 481:
-		words_in_class1.append(element)
-	else:
-		words_in_class2.append(element)
 
-def cal_prob(classn):
-	# Calculate probability for each word in each given classn.
-	# Take a list classn as input, return a dictionary word_probabilities.
-	word_probabilities = dict()
-	for element in classn:
-		word_id = element[1]
-		if word_id in word_probabilities:
-			word_probabilities[word_id] += element[2]
-		else:
-			word_probabilities[word_id] = element[2]
-	length = len(classn)
-	for element in word_probabilities:
-		word_probabilities[element] = float(word_probabilities[element]) / length
-		#word_probabilities[element] = float(word_probabilities[element])
-	return word_probabilities
+class NaiveBayesClassifier(object):
+	"""
+	Implementation of Naive Bayes classifier.
+	"""
 
-# Probabilities of class 1 and class 2.
-#len_train_data = len(train_data)
-#class1_prob = 480.0 / len_train_data
-#class2_prob = float(len_train_data - 480) / len_train_data
+	def __init__(self):
+		# Split g_train_data into 2 classes.
 
-# Calculate probability for each word in each given class.
-# word_prob_class1 is a dictionary with each word in class 1 and its probability,
-# word_prob_class2 is a dictionary with each word in class 2 and its probability.
-word_prob_class1 = cal_prob(words_in_class1)
-word_prob_class2 = cal_prob(words_in_class2)
+		self.words_in_class1 = []
+		self.words_in_class2 = []
+		for element in g_train_data:
+			if element[0] < 481:
+				self.words_in_class1.append(element)
+			else:
+				self.words_in_class2.append(element)
 
-def prob_of_class(classn):
-	# Calculate probability of document for given classn,
-	# take a list classn as input, use formula
-	# P(x1, x2,...., xn | c) = P(x1|c)*P(x2|c)*.....*P(xn|c)
-	# to calculate the probability. Return a dictionary probability.
-	# It will use global variable test_data as test document.
-	probability = 1
-	document = []
-	probabilities = []
-	for element in test_data:
-		if len(document) == 0:
-			document.append(tuple(element))
-		if element[0] == document[0][0]:
-			document.append(tuple(element))
-		else:
-			temp_prob = prob_of_class_helper(document, classn)
+	def cal_prob(self, classn):
+		# Calculate probability for each word in each given classn.
+		# Take a list words with given class classn as input,
+		# return a dictionary word_probabilities, the name is WordID,
+		# the value is the probability of each word in the class.
+
+		word_probabilities = dict()
+		for element in classn:
+			word_id = element[1]  # The 2nd column is WordID.
+			if word_id in word_probabilities:
+				word_probabilities[word_id] += element[2]  # The 3rd column is WordCount.
+			else:
+				word_probabilities[word_id] = element[2]
+		length = float(len(classn))
+		for element in word_probabilities:
+			word_probabilities[element] = word_probabilities[element] / length
+		return word_probabilities
+
+	def prob_of_class(self, classn):
+		# Calculate probabilities of documents for given classn,
+		# take a list classn as input, use formula
+		# P(x1, x2,...., xn | c) = P(x1|c)*P(x2|c)*.....*P(xn|c)
+		# to calculate the probability, logarithm is applied here. 
+		# Return a dictionary probability. It will use global
+		# variable g_test_data as test document.
+
+		document = []
+		probabilities = []
+		for element in g_test_data:
+			if len(document) == 0:
+				document.append(tuple(element))  # Because tuple type is hashable.
+			if element[0] == document[0][0]:
+				document.append(tuple(element))  # Numpy array and list are not hashable.
+			else:
+				temp_prob = self.prob_of_class_helper(document, classn)
+				probabilities.append(temp_prob)
+				document = []
+		if len(document) > 0:
+			temp_prob = self.prob_of_class_helper(document, classn)
 			probabilities.append(temp_prob)
-			document = []
-	if len(document) > 0:
-		probabilities.append(prob_of_class_helper(document, classn))
-	return probabilities
-	
-def prob_of_class_helper(document, classn):
-	# Helper function of prob_of_class.
-	probability = 1.0
-	for word in document:
-		word_id = word[1]
-		word_count = word[2]
-		if word_id in classn:
-			probability *= (classn[word_id]**word_count)
-			#probability += math.log1[(classn[word_id]**word_count)
-		else:
-			probability *= ((1.0/len(classn))**word_count)
-	return probability
+		return probabilities
+		
+	def prob_of_class_helper(self, document, classn):
+		# Helper function of prob_of_class.
+		# Return the probability of each document.
 
-# prob1 and prob2 are the probabilities of documents with given
-# classes class 1 and class 2.
-prob1 = prob_of_class(word_prob_class1)
-prob2 = prob_of_class(word_prob_class2)
+		probability = 0.0
+		for word in document:
+			word_id = word[1]
+			word_count = word[2]
+			if word_id in classn:
+				probability += (math.log(classn[word_id])) * word_count
+			else:
+				# If the word does not exist in the particular class, count
+				# its number of occurrence as 1 because of laplace smoothing.
+				probability += (math.log(1.0/len(classn))) * word_count
+		return probability
 
-# Compare prob1 and prob2 to get predicted_label
-predicted_label = []
-for index in range(len(prob1)):
-	if prob1[index] < prob2[index]:
-		predicted_label.append([2])
-	else:
-		predicted_label.append([1])
+	def compute_accuracy(self):
+		# Classfy g_test_data and compute accuracy
 
-accuracy = 0
-for index in range(len(predicted_label)):
-	if predicted_label[index][0] == test_label[index][0]:
-		accuracy += 1
-accuracy /= float(len(predicted_label))
-print (accuracy)
+		# Calculate probability for each word in each given class.
+		# word_prob_class1 is a dictionary with each word in class 1 and its probability,
+		# word_prob_class2 is a dictionary with each word in class 2 and its probability.
+		word_prob_class1 = self.cal_prob(self.words_in_class1)
+		word_prob_class2 = self.cal_prob(self.words_in_class2)
+
+		# prob1 and prob2 are the probabilities of documents with given
+		# classes class 1 and class 2.
+		prob1 = self.prob_of_class(word_prob_class1)
+		prob2 = self.prob_of_class(word_prob_class2)
+
+		# Compare prob1 and prob2 to get predicted_label
+		predicted_label = []
+		for index in range(len(prob1)):
+			if prob1[index] < prob2[index]:
+				predicted_label.append([2])
+			else:
+				predicted_label.append([1])
+
+		accuracy = 0
+		for index in range(len(predicted_label)):
+			if predicted_label[index][0] == g_test_label[index][0]:
+				accuracy += 1
+		accuracy /= float(len(predicted_label))
+		print "The accuracy of naive bayes classifier is", accuracy
+
+def run():
+	naive_bayes = NaiveBayesClassifier()
+	naive_bayes.compute_accuracy()
+
+run()
